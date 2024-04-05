951545b6b3f34644cab40212641d41f7d423711c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-04-04 23:49:08 -0300

    Add files via upload

--- graficos_com_ggplot2.R
+++ graficos_com_ggplot2.R
@@ -8,4 +8,8 @@
 library(tidyverse) # Pacote para manipulação de dados e gráficos
 library(ggthemes) # Pacote com temas para gráficos em ggplot2
 library(visdat) # Pacote para visualizar dados com NAs
+library(dados) # Pacote com conjunto de dados traduzidos para o português
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
 
